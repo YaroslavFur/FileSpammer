4214d550e07b69e6beae8beb6cfd06554139319b
Yaroslav Fur <tapocki20@gmail.com>
2021-08-27 00:11:45 +0300

    added setOfRows, so program puts not rows by one, but big sets of rows into files. It increased performance by a few times

--- FileSpammer/Main.c
+++ FileSpammer/Main.c
@@ -20,18 +20,40 @@
 // here goes spamming process
 int main()
 {	
-	int spamFiles = SPAM_FILES, spamRows = megabytesToRows(SPAM_FILES, SPAM_MB, strlen(SPAM_ROW));
+	int spamFiles = SPAM_FILES, spamRows = megabytesToRows(SPAM_FILES, SPAM_MB, strlen(SPAM_ROW));	// num of files to create, num of rows in ONE file
 	char spamRow[256] = SPAM_ROW;
 
 	getInput(&spamFiles, &spamRows, spamRow);
 
-	printf("Please wait, spamming...\n\n");
-
 	char* name = (char*)calloc(1 + ZERO_SYMBOL_LENGTH_1, sizeof(char));
 	char* tmpName;
-	FILE* f;
 
+	int rowsInSet;
+	double sizeOfOneFile = rowsToMegabytes(spamFiles, spamRows, strlen(spamRow)) / (double)spamFiles;
+	if (sizeOfOneFile <= 0.01) // if size of one file <= 1 KB
+	{
+		rowsInSet = spamRows;
+	}
+	else if (sizeOfOneFile <= 1) // if size of one file <= 1 MB
+	{
+		rowsInSet = spamRows / 1000;
+	}
+	else // if size of one file >1 MB
+	{
+		rowsInSet = spamRows / 1000;
+	}
+	char* setOfRows = (char*)calloc((strlen(spamRow) + 1) * rowsInSet + ZERO_SYMBOL_LENGTH_1, sizeof(char));
+	char* spamRowWithNewlineCharacter = (char*)calloc((strlen(spamRow) + 1) + ZERO_SYMBOL_LENGTH_1, sizeof(char));
+	spamRowWithNewlineCharacter = strcat(spamRow, "\n");
+	for (int i = 1; i < rowsInSet; i++) // create set of rows because it's much faster to fprintf set of rows that lots of rows by one
+	{
+		setOfRows = strcat(setOfRows, spamRowWithNewlineCharacter);
+	}
+	FILE* f;
 	double percent = 0.0;
+	
+	printf("Please wait, spamming...\n\n");
+
 	printProgressBar(percent, 30, 0);
 
 	for (int i = 1, count = 1; i <= spamFiles; i++)
@@ -56,9 +78,9 @@ int main()
 			break;
 		}
 
-		for (int rowCount = 1; rowCount <= spamRows; rowCount++)
+		for (int rowCount = 1; rowCount <= spamRows; rowCount += rowsInSet)
 		{
-			fprintf(f, "%s\n", spamRow);
+			fprintf(f, "%s\n", setOfRows);
 			while (((i - 1) * spamRows) + rowCount >= ceilMy(spamRows * spamFiles * percent)) // if current printed rows > all rows*percent,
 			{
 				printProgressBar(percent, 30, 0); // print progressBar with this percent
@@ -71,6 +93,7 @@ int main()
 
 		if (i == spamFiles)
 		{
+			printProgressBar(1, 30, 0); // just to be sure :3
 			printf("\n\n");
 			printf("Spammed successfully\n"
 				"Have a great day!\n");
